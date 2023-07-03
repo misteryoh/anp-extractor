78906e850bcb4a2fafb2a57d39b93b177c7f4b63
Misteryoh <oliver.akio@gmail.com>
2023-07-02 22:41:43 -0300

    initial commit

---
+++ anp-sigep-extractor.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.commom.by import By
+from selenium.webdriver.chrome.options import Options
+
+def lambda_handler(event, context):
+    print('Initial Commit')
